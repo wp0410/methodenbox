c301aa99af7ab75398abb8b868c1cfd06cdc0b10
wp0410 <walter.pachlinger@gmail.com>
2019-03-25 08:27:48 +0000

    Handle PHP USER_WARNING

---
+++ mbx/model/app_warning.php
@@ -0,0 +1,38 @@
+<?php
+//---------------------------------------------------------------------------------------
+//  Copyright (c) 2018 Walter Pachlinger (walter.pachlinger@gmx.at)
+//    
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this 
+//  file except in compliance with the License. You may obtain a copy of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software distributed under 
+//  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF 
+//  ANY KIND, either express or implied. See the License for the specific language 
+//  governing permissions and limitations under the License.
+//----------------------------------------------------------------------------------------
+function handle_warning(int $errno, string $errstr, string $errfile = null, int $errline = null, array $errcontext = null)
+{
+    $result = false;
+    
+    switch($errno)
+    {
+        case E_USER_WARNING:
+            // TO DO: Log the warning
+            $result = true;
+            break;
+        case E_USER_NOTICE:
+            // TO DO: Log the notice
+            $result = true;
+            break;
+        default:
+            break;
+    }
+    
+    return $result;
+}
+
+function set_warning_handler()
+{
+    set_error_handler(handle_warning, E_USER_WARNING | E_USER_NOTICE);
+}
+?>
